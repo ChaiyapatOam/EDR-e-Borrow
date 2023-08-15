af981f1d39cdfe4bf020009653cf4600fef36718
SSSBoOm <boom1909chanapat@gmail.com>
2023-08-15 15:17:45 +0700

    ✨feat: add category filter

--- src/components/admin/AdminProductList.svelte
+++ src/components/admin/AdminProductList.svelte
@@ -13,20 +13,74 @@
             console.log(error);
         }
     });
+    let categoryList = ["All", "Components", "Tools"];
+    $: state = 0;
+
+    const changeIndex = (idx: number) => {
+        state = idx;
+    };
 </script>
+
+<div class="p-4">
+    <h1 class="text-2xl text-orange font-bold">แก้ไขคลังของ</h1>
+    <div class="flex items-center py-4 overflow-auto whitespace-nowrap">
+        {#each categoryList as category, index}
+            <button
+                type="button"
+                on:click={() => changeIndex(index)}
+                class={state === index
+                    ? "text-white hover:text-black bg-orange  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    : "text-white hover:text-black bg-navy  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"}
+                >{category}</button
+            >
+        {/each}
+    </div>
+</div>
 
 <section>
     <div class="flex flex-wrap justify-center mx-auto gap-y-4 md:gap-x-4 px-6">
         {#if products}
+            {#if state === 0}
+                {#each products as product}
+                    <AdminProductCard
+                        id={product.id}
+                        uid={product.category + "-" + product.id}
+                        name={product.name}
+                        image={product.image}
+                        quantity={product.quantity}
+                        category={product.category}
+                    />
+                {/each}
+            {/if}
+        {/if}
+
+        {#if state === 1}
             {#each products as product}
-                <AdminProductCard
-                    id={product.id}
-                    uid={product.category + "-" + product.id}
-                    name={product.name}
-                    image={product.image}
-                    quantity={product.quantity}
-                    category={product.category}
-                />
+                {#if product.category == "EDREC"}
+                    <AdminProductCard
+                        id={product.id}
+                        uid={product.category + "-" + product.id}
+                        name={product.name}
+                        image={product.image}
+                        quantity={product.quantity}
+                        category={product.category}
+                    />
+                {/if}
+            {/each}
+        {/if}
+
+        {#if state === 2}
+            {#each products as product}
+                {#if product.category == "EDRET"}
+                    <AdminProductCard
+                        id={product.id}
+                        uid={product.category + "-" + product.id}
+                        name={product.name}
+                        image={product.image}
+                        quantity={product.quantity}
+                        category={product.category}
+                    />
+                {/if}
             {/each}
         {:else}
             <h1 class="text-orange text-center font-bold p-4">Nothing</h1>
--- src/pages/admin/Admin.svelte
+++ src/pages/admin/Admin.svelte
@@ -1,14 +1,11 @@
 <script>
-  import Navbar from "@/components/admin/Navbar.svelte";
-  import AdminProductCard from "@/components/admin/AdminProductCard.svelte";
-  import CategoryList from "@/components/admin/CategoryList.svelte";
-  import Hero from "@/components/admin/Hero.svelte";
-  import AdminProductList from "@/components/admin/AdminProductList.svelte";
+    import Navbar from "@/components/admin/Navbar.svelte";
+    import Hero from "@/components/admin/Hero.svelte";
+    import AdminProductList from "@/components/admin/AdminProductList.svelte";
 </script>
 
 <div>
-  <Navbar />
-  <Hero />
-  <CategoryList />
-  <AdminProductList />
+    <Navbar />
+    <Hero />
+    <AdminProductList />
 </div>
